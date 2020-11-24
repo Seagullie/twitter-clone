48c792e1320523aaa7996af32a1d10d75114b27d
Seagullie <59409233+Seagullie@users.noreply.github.com>
2020-11-24 21:01:32 +0200

    added search user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
     before_action :user
+    before_action :get_is_following, except: [:search]
+    # before_action :is_following
     
 
     def profile
-        @is_following = is_following
-
         user_handle = params[:handle] 
         @user = user =  User.find_by(username: user_handle)
         if current_user == user
@@ -36,10 +36,49 @@ class UsersController < ApplicationController
         profile
     end
 
-    def is_following
-        association_present = Follow.where(user_id: current_user.id, is_following: @user.id).length != 0
+    def get_is_following
+        # console
+        if not current_user
+            @is_following = false
+            return true
+        end
 
-        return association_present
+        @is_following = Follow.where(user_id: current_user.id, is_following: @user.id).length != 0
+        # byebug
+        return true
+    end
+
+    def follow_list
+        @follow_list = @user.follows.map {|follow| User.find(follow.is_following)}
+        # byebug
+        render '/users/follow_list', user: @user
+    end
+
+    def followers
+        @followers = Follow.where(is_following: @user.id).map {|follow| User.find(follow.user_id)}
+        # byebug
+        render '/users/followers', user: @user
+    end
+
+
+    def search
+        search_query = params[:search_data]
+        results = search_for_user(search_query)
+        p results
+        # byebug
+        if results.length == 0
+            render js: "  M.toast({html: 'Failed to find the user', classes: 'red'})"
+            return false
+        end
+        formatted_results = results[0].name + " " + results[0].username
+        p formatted_results
+        js_to_render = %Q(alert("Test Successfull, you are trying to search for #{formatted_results}"))
+        p js_to_render
+        render js: "window.location.href = '/#{results[0].username}';"
+        
+
+        # redirect_to "/#{results[0].username}"
+
     end
 
     private
@@ -48,4 +87,11 @@ class UsersController < ApplicationController
         target_handle = params[:handle]
         @user =  User.find_by(username: target_handle)
     end
+
+    def search_for_user(data)
+        users = User.where(name: data) + User.where(username: data)
+
+        return users
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :tweeets
   root 'tweeets#index'
   get '/:handle', to: 'users#profile', as: 'user_handle'
+  get '/users/:handle/followList', to: 'users#follow_list'
+  get '/users/:handle/followers', to: 'users#followers'
   post '/users/:handle/follow', to: 'users#follow'
   post '/users/:handle/unfollow', to: 'users#unfollow'
+  get '/users/search', to: 'users#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
